b7c9d4dd4a16e5844a28e90e41a55e33ff2cc74f
Naruyuki Oyama <narutaso.oym@gmail.com>
2025-03-04 17:18:20 +0900

    追加されてた分を一旦コメントアウト

--- scripts/dryrun.sh
+++ scripts/dryrun.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-terraform init -var USERID=${userID} -input=false -no-color -backend-config="key=terraform.tfstate" -backend-config="bucket=zukkie-terraform-state"
+terraform init -var USERID=${userID} -input=false -no-color -backend-config="key=terraform.tfstate" -backend-config="narutaso-bucket"
 terraform validate -no-color
-tfcmt plan -- terraform plan -no-color -var USERID=${userID}
+# tfcmt plan -- terraform plan -no-color -var USERID=${userID}
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,19 +3,19 @@
 set -ex
 
 TERRAFORM_VERSION="1.5.4"
-TFCMT_VERSION="v4.4.3"
-TFLINT_VERSION="v0.47.0"
+# TFCMT_VERSION="v4.4.3"
+# TFLINT_VERSION="v0.47.0"
 
 wget -q "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 unzip -d /usr/local/bin "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 
-wget "https://github.com/suzuki-shunsuke/tfcmt/releases/download/${TFCMT_VERSION}/tfcmt_linux_amd64.tar.gz" -P /tmp
-tar zxvf /tmp/tfcmt_linux_amd64.tar.gz -C /tmp
-mv /tmp/tfcmt /usr/local/bin/tfcmt
+# wget "https://github.com/suzuki-shunsuke/tfcmt/releases/download/${TFCMT_VERSION}/tfcmt_linux_amd64.tar.gz" -P /tmp
+# tar zxvf /tmp/tfcmt_linux_amd64.tar.gz -C /tmp
+# mv /tmp/tfcmt /usr/local/bin/tfcmt
 
-# GITHUB_TOKENを使って401が出るので削除
-unset GITHUB_TOKEN
-curl -s https://raw.githubusercontent.com/terraform-linters/tflint/${TFLINT_VERSION}/install_linux.sh | bash
-# TFLINT_LOG=debug
-tflint --init
-tflint
+# # GITHUB_TOKENを使って401が出るので削除
+# unset GITHUB_TOKEN
+# curl -s https://raw.githubusercontent.com/terraform-linters/tflint/${TFLINT_VERSION}/install_linux.sh | bash
+# # TFLINT_LOG=debug
+# tflint --init
+# tflint
