af517d824b4ca74e6080bfbac2db9d29701f6385
Naruyuki Oyama <narutaso.oym@gmail.com>
2025-03-06 16:05:20 +0900

    hhhhhhhhhhh

--- scripts/dryrun.sh
+++ scripts/dryrun.sh
@@ -4,5 +4,6 @@ set -ex
 # terraform init -var USERID=458485069334 -input=false -no-color -backend-config="key=terraform.tfstate" -backend-config="bucket=narutaso-bucket"
 terraform init -input=false -no-color -backend-config="key=terraform.tfstate" -backend-config="bucket=narutaso-bucket"
 terraform validate -no-color
+terraform plan -no-color
 # tfcmt plan -- terraform plan -no-color -var USERID=${userID}
 # tfcmt plan -- terraform plan -no-color -var USERID=458485069334
